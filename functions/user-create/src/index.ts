41b11b7989e83a9f6eb561daabefb6e281fb85b0
Ravisankar Ka <52249806+Ravi-ka@users.noreply.github.com>
2026-03-10 12:00:59 +0530

    Delete functions directory

--- functions/user-create/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-
-
-export const createUser = async (req: Request, res: Response) => {
-    res.status(201).json({ message: 'User created successfully' });
-};
--- functions/user-update/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-
-
-export const updateUser = async (req: Request, res: Response) => {
-    res.status(200).json({ message: 'User updated successfully' });
-};
